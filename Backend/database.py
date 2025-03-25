351d501efab0aef51a9cd69943ae8e949fcd43ef
Gabriel Alves Gama <gabrielgama0208@gmail.com>
2025-03-25 09:44:07 -0300

    adicionado .env

--- Backend/database.py
+++ Backend/database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-user = "postgres"
-password = "admin"
-database = "pdsi2_bd"
-host = "localhost"
-SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}/{database}"
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
